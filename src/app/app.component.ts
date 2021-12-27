0f6cc3e7383b7178d9cd1651480786cf0a86c2c9
AJ <ancheetah@users.noreply.github.com>
2021-12-27 12:41:32 -0800

    angular component basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // where the component will be injected into index.html
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hitesh-crash';
+  title = 'Hitesh Angular Crash Course Youtube';
+  name = '@ancheetah';
+  date = new Date();
+
+  constructor() {
+    // this is the first thing called when an object is instantiated
+    console.log("constructor called");
+    this.doSomething('Some Text');
+  }
+
+  doSomething(val: string): void {
+    // this is a method of the AppComponent class
+    val = "Awesome";
+  }
+
 }
+ 
